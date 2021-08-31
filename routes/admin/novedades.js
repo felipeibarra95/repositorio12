6cb733ca22ddd1ed188a6ce331c5274d34ce3883
Felipe Ibarra <feliequis00@gmail.com>
2021-08-31 12:57:31 -0300

    31/08/21

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 /* var usuariosModel = require('./../../models/usuariosModel');
  */
 
-router.get('/', function(req, res, next) {
-    res.render('admin/novedades', {
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();  
+  res.render('admin/novedades', {
       layout: 'admin/layout',
-      usuario: req.session.nombre
+      usuario: req.session.nombre,
+      novedades
     });
   });
+
+router.get('/eliminar/:id', async (req, res, next) =>{
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+}); 
+
 
   module.exports = router;
